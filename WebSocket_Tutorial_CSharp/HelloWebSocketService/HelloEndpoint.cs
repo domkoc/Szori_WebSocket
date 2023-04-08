0af43e1fdde2b4b84347dc0ee15b382c15243e96
Dominik Kocka <kocka98@gmail.com>
2023-04-08 12:45:11 +0200

    Add starter project

---
+++ WebSocket_Tutorial_CSharp/HelloWebSocketService/HelloEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.WebSockets;
+namespace HelloWebSocketService
+{
+    public class HelloEndpoint
+    {
+        public async Task Open(WebSocket socket)
+        {
+            Console.WriteLine("WebSocket opened.");
+        }
+        public async Task Close(WebSocket socket)
+        {
+            Console.WriteLine("WebSocket closed.");
+        }
+        public async Task Error(WebSocket socket, Exception ex)
+        {
+            Console.WriteLine("WebSocket error: " + ex.Message);
+        }
+        public async Task<string> Message(WebSocket socket, string message)
+        {
+            Console.WriteLine($"WebSocket message: {message}");
+            return $"Hello: {message}";
+        }
+    }
+}
---
+++ WebSocket_Tutorial_CSharp/HelloWebSocketService/Program.cs
@@ -0,0 +1,8 @@
+using HelloWebSocketService;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<HelloEndpoint>();
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseWebSockets();
+app.UseMiddleware<WebSocketMiddleware>();
+app.Run();
